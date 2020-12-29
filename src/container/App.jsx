a36dfe01cce8cd7cf693feae7fa20539e57ae145
Diego Rojas <diegorojas431@gmail.com>
2020-12-28 22:46:23 -0500

    Construyendo el banner

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 import '../assets/styles/App.scss';
-import Cloud from '../assets/images/cloud.png';
+import cloud from '../assets/images/cloud.png';
+import banner from '../assets/images/banner__image.jpg';
 
 const App = () => (
-  <section>
-    <h1>Hello world!</h1>
-    <img src={Cloud} alt='nube' />
+  <section className='container'>
+    <article className='banner'>
+      <img src={banner} alt='banner' />
+    </article>
+
+    <section className='main__information'>
+      <article className='main__information-icon'>
+
+        <img src={cloud} alt='icon' />
+      </article>
+      <article className='main__information-temperature'>
+        <h1>30 C°</h1>
+      </article>
+    </section>
   </section>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const loader = require('sass-loader');
 
 module.exports = {
   entry: './src/Index.js',
